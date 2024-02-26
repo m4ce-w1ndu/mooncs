ffe3f1984a6d5ad61c677e665ed5d0d1ce14d14a
m4ce-w1ndu <m4ce.w1ndu@icloud.com>
2024-02-27 00:30:58 +0100

    Added attributes for bitfield conversion

--- AGCCore/Core/Attributes.cs
+++ AGCCore/Core/Attributes.cs
@@ -1,7 +1,11 @@
 using System;
+using System.Reflection;
 
 namespace AGCCore.Core
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public sealed class BitfieldLengthAttribute : Attribute
     {
         public uint Length { get; init; }
@@ -9,6 +13,33 @@ namespace AGCCore.Core
         public BitfieldLengthAttribute(uint length)
         {
             Length = length;
+        }
+    }
+
+    internal class ConversionUtility
+    {
+        public static long ToLong<T>(T t) where T: struct
+        {
+            long result = 0;
+            int offset = 0;
+
+            foreach (var field in t.GetType().GetFields())
+            {
+                var attrs = field.GetCustomAttributes(typeof(BitfieldLengthAttribute), false);
+                if (attrs.Length == 1)
+                {
+                    var fieldLength = ((BitfieldLengthAttribute)attrs[0]).Length;
+                    long mask = 0;
+                    for (int i = 0; i < fieldLength; i++)
+                        mask |= 1 << i;
+
+                    result |= ((uint)field.GetValue(t) & mask) << offset;
+
+                    offset += (int)fieldLength;
+                }
+            }
+
+            return result;
         }
     }
 }
